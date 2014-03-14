6fa6698939ff224eeb231445312749587bc15851
Joel Teichroeb <joel@teichroeb.net>
2014-03-14 13:30:15 -0700

    Add more features

--- ass2/PingClient.java
+++ ass2/PingClient.java
@@ -14,23 +14,59 @@ public class PingClient
 		int port;
 		int i = 0;
 		Timer timer;
+		int packets_received = 0;
+		double min = 1000;
+		double max = 0;
+		double total = 0;
+		int mdev = 0;
+		long ping_start_time;
+		long current_send_time = 0;
+		int current_timeout = 1000;
 		public SendTask(String host, int p, Timer t) {
 			try {
 				socket = new DatagramSocket();
 				address = InetAddress.getByName(host);
 				port = p;
 				timer= t;
-				socket.setSoTimeout(1000);
+				ping_start_time = System.currentTimeMillis();
 			} catch(Exception e) {
 			}
 		}
+		private void getPacket() {
+				byte[] buf = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				try {
+					socket.receive(packet);
+					double delta = (System.nanoTime() - current_send_time) / (double)(1000 * 1000);
+					if (buf[5] - '0' != i) {
+						current_timeout -= delta;
+						socket.setSoTimeout(current_timeout);
+						getPacket();
+						return;
+					}
+					if (delta < min) {
+						min = delta;
+					}
+					if (delta > max) {
+						max = delta;
+					}
+					total += delta;
+					System.out.println(packet.getLength() +" bytes from " +
+										packet.getAddress().getHostAddress() + 
+										": seq="+i+" time="+ delta+" ms");
+					++packets_received;
+				} catch(Exception e) {
+					System.out.println("Timeout");
+				}
+		}
 		public void run() {
 			try {
+				current_timeout = 1000;
+				socket.setSoTimeout(current_timeout);
 				byte[] buf;
 				Date d = new Date();
-				long send_time = System.currentTimeMillis();
-				String data = "PING " + i + " "  +send_time+"\r\n";
-				System.out.println("Sending: "+ data);
+				current_send_time = System.nanoTime();
+				String data = "PING " + i + " "  +new Date()+"\r\n";
 				buf = data.getBytes();
 				// Create a datagram packet to hold incomming UDP packet.
 				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
@@ -38,18 +74,20 @@ public class PingClient
 				// Block until the host receives a UDP packet.
 				socket.send(packet);
 
-				buf = new byte[1024];
-				packet = new DatagramPacket(buf, buf.length);
-				try {
-					socket.receive(packet);
-					long delta = System.currentTimeMillis() - send_time;
-					System.out.println(delta);
-					printData(packet);
-				} catch(Exception e) {
-				}
+				getPacket();
 				// Print the recieved data.
 				++i;
 				if (i == 10) {
+					System.out.println("--- ping statistics ---");
+					int loss = ((10-packets_received)*100)/10;
+					System.out.println("10 packets transmitted, "+packets_received+
+										" received, "+loss+"% packet loss, time "+
+										(System.currentTimeMillis() - ping_start_time) +" ms");
+					if (packets_received != 0) {
+						double avg = (total/packets_received);
+						System.out.println("rtt min/avg/max = "+min+"/"+avg+
+											"/"+max+" ms");
+					}
 					cancel();
 					timer.cancel();
 				}
@@ -69,43 +107,12 @@ public class PingClient
 		String host = args[0];
 		int port = Integer.parseInt(args[1]);
 
+		System.out.println("PING " + host);
 		// Create a datagram socket for receiving and sending UDP packets
 		// through the port specified on the command line.
 
 		Timer t = new Timer();
 		SendTask t2 = new SendTask(host, port, t);
 		t.schedule(t2, 1000, 1000);
-	}
-
-	/*
-	 * Print ping data to the standard output stream.
-	 */
-	private static void printData(DatagramPacket request) throws Exception
-	{
-		// Obtain references to the packet's array of bytes.
-		byte[] buf = request.getData();
-
-		// Wrap the bytes in a byte array input stream,
-		// so that you can read the data as a stream of bytes.
-		ByteArrayInputStream bais = new ByteArrayInputStream(buf);
-
-		// Wrap the byte array output stream in an input stream reader,
-		// so you can read the data as a stream of characters.
-		InputStreamReader isr = new InputStreamReader(bais);
-
-		// Wrap the input stream reader in a bufferred reader,
-		// so you can read the character data a line at a time.
-		// (A line is a sequence of chars terminated by any combination of \r and \n.)
-		BufferedReader br = new BufferedReader(isr);
-
-		// The message data is contained in a single line, so read this line.
-		String line = br.readLine();
-
-		// Print host address and data received from it.
-		System.out.println(
-			"Received from " +
-			request.getAddress().getHostAddress() +
-			": " +
-			new String(line) );
 	}
 }
