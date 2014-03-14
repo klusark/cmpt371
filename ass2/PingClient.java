d86def066a0a3fb932b42045ae490cacbcea9b37
Joel Teichroeb <joel@teichroeb.net>
2014-03-14 13:43:50 -0700

    Client improvments

--- ass2/PingClient.java
+++ ass2/PingClient.java
@@ -12,7 +12,7 @@ public class PingClient
 		DatagramSocket socket;
 		InetAddress address;
 		int port;
-		int i = 0;
+		int current_seq = 0;
 		Timer timer;
 		int packets_received = 0;
 		double min = 1000;
@@ -22,15 +22,12 @@ public class PingClient
 		long ping_start_time;
 		long current_send_time = 0;
 		int current_timeout = 1000;
-		public SendTask(String host, int p, Timer t) {
-			try {
-				socket = new DatagramSocket();
-				address = InetAddress.getByName(host);
-				port = p;
-				timer= t;
-				ping_start_time = System.currentTimeMillis();
-			} catch(Exception e) {
-			}
+		public SendTask(InetAddress addr, int p, Timer t) throws SocketException {
+			socket = new DatagramSocket();
+			address = addr;
+			port = p;
+			timer= t;
+			ping_start_time = System.currentTimeMillis();
 		}
 		private void getPacket() {
 				byte[] buf = new byte[1024];
@@ -38,7 +35,10 @@ public class PingClient
 				try {
 					socket.receive(packet);
 					double delta = (System.nanoTime() - current_send_time) / (double)(1000 * 1000);
-					if (buf[5] - '0' != i) {
+
+					// Make sure that this packet has the same seq as the one that was sent
+					if (buf[5] - '0' != current_seq) {
+						// If it isn't valid, we need to change the timeout and receive some more
 						current_timeout -= delta;
 						socket.setSoTimeout(current_timeout);
 						getPacket();
@@ -53,9 +53,9 @@ public class PingClient
 					total += delta;
 					System.out.println(packet.getLength() +" bytes from " +
 										packet.getAddress().getHostAddress() + 
-										": seq="+i+" time="+ delta+" ms");
+										": seq="+current_seq+" time="+ delta+" ms");
 					++packets_received;
-				} catch(Exception e) {
+				} catch(IOException e) {
 					System.out.println("Timeout");
 				}
 		}
@@ -66,18 +66,18 @@ public class PingClient
 				byte[] buf;
 				Date d = new Date();
 				current_send_time = System.nanoTime();
-				String data = "PING " + i + " "  +new Date()+"\r\n";
+				String data = "PING " + current_seq + " "  +new Date()+"\r\n";
 				buf = data.getBytes();
 				// Create a datagram packet to hold incomming UDP packet.
 				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 
-				// Block until the host receives a UDP packet.
 				socket.send(packet);
 
+				// Block for up to a second waiting for a packet
 				getPacket();
-				// Print the recieved data.
-				++i;
-				if (i == 10) {
+				++current_seq;
+				if (current_seq == 10) {
+					// All 10 have been sent, print stats
 					System.out.println("--- ping statistics ---");
 					int loss = ((10-packets_received)*100)/10;
 					System.out.println("10 packets transmitted, "+packets_received+
@@ -88,7 +88,8 @@ public class PingClient
 						System.out.println("rtt min/avg/max = "+min+"/"+avg+
 											"/"+max+" ms");
 					}
-					cancel();
+
+					// No need for the timer anymore
 					timer.cancel();
 				}
 			} catch(Exception e) {
@@ -105,14 +106,33 @@ public class PingClient
 			return;
 		}
 		String host = args[0];
-		int port = Integer.parseInt(args[1]);
+		int port = 0;
+
+		// Check to make sure the port makes sense
+		try {
+			port = Integer.parseInt(args[1]);
+			if (port < 1 || port > 65535) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			System.out.println("Invalid port");
+			return;
+		}
+
+		// Check the host as well
+		InetAddress address;
+		try {
+			address = InetAddress.getByName(host);
+		} catch (UnknownHostException e) {
+			System.out.println("Invalid host");
+			return;
+		}
 
 		System.out.println("PING " + host);
-		// Create a datagram socket for receiving and sending UDP packets
-		// through the port specified on the command line.
 
+		// Create and setup timer
 		Timer t = new Timer();
-		SendTask t2 = new SendTask(host, port, t);
+		SendTask t2 = new SendTask(address, port, t);
 		t.schedule(t2, 1000, 1000);
 	}
 }
